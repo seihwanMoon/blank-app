7222054f24c2b66652790979b35180c3ba80507a
seihwanMoon <skimoon@naver.com>
2025-01-09 12:49:09 +0000

    t2

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 st.title("🎈 나의 APP")
 st.markdown("""
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    # Let's start building! 
+    For help and inspiration, 
+    head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 """
 )
